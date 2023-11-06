2680f60b182522d3668a94ba153f08325150d617
Takafumi Asahara <asabon.t@gmail.com>
2023-11-07 06:43:27 +0900

    version up to v1.0.1

--- PFD_Editor/Properties/AssemblyInfo.cs
+++ PFD_Editor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("PFD_Editor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PFD Editor")]
+[assembly: AssemblyDescription("PFD (Process Flow Diagram) Editor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PFD_Editor")]
+[assembly: AssemblyProduct("PFD Editor")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
