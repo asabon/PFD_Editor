75540b14f246408944a8e096e0cc895e4afa6681
Takafumi Asahara <asabon.t@gmail.com>
2023-11-10 07:59:34 +0900

    modify

---
+++ UnitTestProject/TestJsonFile.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PFD_Editor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestJsonFile
+    {
+        [TestMethod]
+        public void Test_Constructor()
+        {
+            PFD_Editor.JsonFile jsonFile = new JsonFile("sample.json");
+        }
+    }
+}
