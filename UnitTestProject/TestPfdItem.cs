b819ce3337cb15eeffa8d1f4118f51bfe8431886
Takafumi Asahara <asabon.t@gmail.com>
2023-11-11 06:50:04 +0900

    modify

--- UnitTestProject/TestPfdItem.cs
+++ UnitTestProject/TestPfdItem.cs
@@ -28,30 +28,5 @@ namespace UnitTestProject
             Assert.AreEqual("", pfdItem.description);
             Assert.AreEqual(PfdItem.PfdItemType.WorkProduct, pfdItem.type);
         }
-
-        [TestMethod]
-        public void TestMethod_PfdWorkProduct_Constructor_01()
-        {
-            PFD_Editor.PfdWorkProduct pfdWorkProduct = new PFD_Editor.PfdWorkProduct();
-            Assert.AreEqual(0, pfdWorkProduct.id);
-            Assert.AreEqual("", pfdWorkProduct.subject);
-            Assert.AreEqual("", pfdWorkProduct.description);
-            Assert.AreEqual(PfdItem.PfdItemType.WorkProduct, pfdWorkProduct.type);
-        }
-
-        [TestMethod]
-        public void TestMethod_PfdWorkProduct_Constructor_02()
-        {
-            PFD_Editor.PfdWorkProduct pfdWorkProductOriginal = new PFD_Editor.PfdWorkProduct();
-            pfdWorkProductOriginal.id = 1;
-            pfdWorkProductOriginal.subject = "aaa";
-            pfdWorkProductOriginal.description = "bbb";
-            PFD_Editor.PfdWorkProduct pfdWorkProduct = new PFD_Editor.PfdWorkProduct(pfdWorkProductOriginal);
-            Assert.AreEqual(pfdWorkProductOriginal.id, pfdWorkProduct.id);
-            Assert.AreEqual(pfdWorkProductOriginal.subject, pfdWorkProduct.subject);
-            Assert.AreEqual(pfdWorkProductOriginal.description, pfdWorkProduct.description);
-            Assert.AreEqual(PfdItem.PfdItemType.WorkProduct, pfdWorkProduct.type);
-        }
-
     }
 }
