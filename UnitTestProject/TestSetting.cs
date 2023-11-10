fa31126a605a4159c9afc3a41b0a879722b5c475
Takafumi Asahara <asabon.t@gmail.com>
2023-11-11 08:39:44 +0900

    add TestSetting.cs

---
+++ UnitTestProject/TestSetting.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PFD_Editor;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestSetting
+    {
+        private const string settingFileName = "testSetting.ini";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            PFD_Editor.Setting setting = new Setting(settingFileName);
+        }
+    }
+}
