69af02b2150ed4e87bac6556968d55aabaebd045
Takafumi Asahara <asabon.t@gmail.com>
2023-11-11 06:45:05 +0900

    modify

---
+++ UnitTestProject/TestPfdWorkProduct.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestPfdWorkProduct
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
