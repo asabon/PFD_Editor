972dbf8fa79c81a5f040714e9bc07edeb03dcc7d
Takafumi Asahara <asabon.t@gmail.com>
2023-11-11 11:28:08 +0900

    Add TestMainForm.cs

---
+++ UnitTestProject/TestMainForm.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PFD_Editor;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestMainForm
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            PFD_Editor.MainForm mainForm = new PFD_Editor.MainForm();
+        }
+    }
+}
